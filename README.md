59914ce335062085dd7d3150c700b8e8cab23c7f
Te0code <teodor.lzr@gmai.com>
2021-08-19 20:59:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Directory tree generator 
+
+This CLI application will take a directory path as an argument at the command line and display tree diagram on console.
+
+
---
+++ tree.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# tree.py
+
+""" This module provides Directory Tree entry poiny script """
+
+from dirgen.cli import main
+
+if __name__ == "__main__":
+    main()
+
+
